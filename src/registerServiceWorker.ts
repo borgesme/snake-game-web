a27e8fc9a3032257acdde917306de4314fd0cf06
borgesme <jdzhao.me@gmail.com>
2026-06-10 21:42:37 +0800

    feat: 添加 PWA 支持

---
+++ src/registerServiceWorker.ts
@@ -0,0 +1,16 @@
+export function registerServiceWorker() {
+  if (!('serviceWorker' in navigator) || import.meta.env.DEV) {
+    return;
+  }
+
+  window.addEventListener('load', () => {
+    const serviceWorkerUrl = new URL(
+      `${import.meta.env.BASE_URL}service-worker.js`,
+      window.location.origin,
+    );
+
+    navigator.serviceWorker.register(serviceWorkerUrl, {
+      scope: import.meta.env.BASE_URL,
+    });
+  });
+}
